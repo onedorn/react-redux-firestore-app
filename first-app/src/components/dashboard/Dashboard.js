53bcb091a1518de3388784cfaf03ac9e3c75b284
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-06 18:50:37 +0300

    Created ProjectSummary Component to make entire ProjectList component more modular and neat.

--- first-app/src/components/dashboard/Dashboard.js
+++ first-app/src/components/dashboard/Dashboard.js
@@ -6,13 +6,14 @@ class Dashboard extends React.Component {
     render() {
         return (
             <div className="dashboard container">
-                <div className="row"></div>
-                <div className="col s12 m6">
-                <ProjectList />
+                <div className="row">
+                    <div className="col s12 m6">
+                        <ProjectList />
+                    </div>
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications />   
+                    </div>
                 </div>
-                <div className="col s12 m5 offset-m1"></div>
-                <Notifications />
-                
             </div>
         )
     }
