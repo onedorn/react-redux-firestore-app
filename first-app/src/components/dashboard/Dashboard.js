0f1f6c85922161d6617cbb744f3ddddc2f1da1ee
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-07 23:09:41 +0300

    Syncing Data with Firestore

--- first-app/src/components/dashboard/Dashboard.js
+++ first-app/src/components/dashboard/Dashboard.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+
 
 
 class Dashboard extends React.Component {
@@ -25,9 +28,15 @@ class Dashboard extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+    console.log(state)
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(Dashboard)
