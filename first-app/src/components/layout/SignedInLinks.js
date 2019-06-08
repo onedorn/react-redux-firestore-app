9a09cd1310ddf603d4f58ad560649a1e6a42e579
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-08 22:33:20 +0300

    Succesfully logged out of the application

--- first-app/src/components/layout/SignedInLinks.js
+++ first-app/src/components/layout/SignedInLinks.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
 
-function SignInLinks() {
+const SignInLinks = (props) => { 
     return(
         <ul className='right'>
             <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><a onClick= {props.signOut} >Log Out</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NaN</NavLink></li>
         </ul>
     )
 }
 
-export default SignInLinks
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignInLinks)
--- first-app/src/store/actions/authActions.js
+++ first-app/src/store/actions/authActions.js
@@ -1,3 +1,5 @@
+import { getFirebase } from "react-redux-firebase";
+
 export const signIn = (credentials) => {
     return (dispatch, getState, { getFirebase }) => {
         const firebase = getFirebase();
@@ -10,5 +12,15 @@ export const signIn = (credentials) => {
         }).catch((err) => {
             dispatch({ type: 'LOGIN_ERROR', err});
         });
+    }
+}
+
+export const signOut = () => {
+    return (dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+
+        firebase.auth().signOut().then(() => {
+            dispatch({ type: 'SIGNOUT_SUCCESS'})
+        })
     }
 }
