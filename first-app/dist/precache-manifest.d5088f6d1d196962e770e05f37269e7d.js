805847198b401c7b0496c15f339dc5251e1812b7
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-10 03:21:30 +0300

    Deploying to Firebase Hosting

---
+++ first-app/dist/precache-manifest.d5088f6d1d196962e770e05f37269e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3da1af11cb8821ca88438db2219a696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468c08265c4f29eebc41",
+    "url": "/static/css/main.432de035.chunk.css"
+  },
+  {
+    "revision": "b7013d99b07da7384fda",
+    "url": "/static/js/2.2533244e.chunk.js"
+  },
+  {
+    "revision": "468c08265c4f29eebc41",
+    "url": "/static/js/main.1c85df10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
